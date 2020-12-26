4a696849bb85be11408a853fec54b6f887821927
Malek Zarzour <malek.zarzour@gmail.com>
2020-12-26 18:42:10 +0100

    configured routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-// import components
-import Header from './components/Header/index';
+// Layout
+import MainLayout from './layout/MainLayout';
+
+// Pages
 import Homepage from './pages/Homepage/index';
+import Registration from './pages/Registration/index';
 
-// import styling
+// Styling
 import './default.scss';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="main">
-        <Homepage />
-      </div>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <MainLayout>
+              <Homepage />
+            </MainLayout>
+          )}
+        />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <MainLayout>
+              <Registration />
+            </MainLayout>
+          )}
+        />
+      </Switch>
     </div>
   );
 }
