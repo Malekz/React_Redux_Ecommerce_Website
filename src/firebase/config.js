664f9bb2cbbe043242b0ed90f94652e2349a679e
Malek Zarzour <malek.zarzour@gmail.com>
2020-12-28 23:58:14 +0100

    created firebase config and utils

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBgYIZnnsNUj5RyQJGN_eQ3g4oIOn-eiVU',
+  authDomain: 'gasby-and-gasby.firebaseapp.com',
+  projectId: 'gasby-and-gasby',
+  storageBucket: 'gasby-and-gasby.appspot.com',
+  messagingSenderId: '840443406872',
+  appId: '1:840443406872:web:8d505516d6a19c2726f16d',
+};
