20df05e4d7b2b055773e52510f89bef261ee392e
Malek Zarzour <malek.zarzour@gmail.com>
2020-12-31 22:44:07 +0100

    firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,22 +1,8 @@
-export const firebaseDevConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+export var firebaseConfig = {
+  apiKey: 'AIzaSyBgYIZnnsNUj5RyQJGN_eQ3g4oIOn-eiVU',
+  authDomain: 'gasby-and-gasby.firebaseapp.com',
+  projectId: 'gasby-and-gasby',
+  storageBucket: 'gasby-and-gasby.appspot.com',
+  messagingSenderId: '840443406872',
+  appId: '1:840443406872:web:8d505516d6a19c2726f16d',
 };
-
-export const firebaseProdConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-};
-
-export const firebaseConfig =
-  process.env.NODE_ENV === 'production'
-    ? firebaseProdConfig
-    : firebaseDevConfig;
