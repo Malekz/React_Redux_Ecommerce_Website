16be67808144d48b061e7cc1468315cbbeb2f22a
Malek Zarzour <malek.zarzour@gmail.com>
2021-01-02 23:08:33 +0100

    created Login component

---
+++ src/pages/Login/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Componnents
+import SignIn from './../../components/SignIn';
+
+// Styling
+import './styles.scss';
+
+const Login = (props) => {
+  return <SignIn />;
+};
+export default Login;
