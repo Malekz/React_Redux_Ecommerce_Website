aca9eabacb89045098a98252127e028e56c149b6
Malek Zarzour <malek.zarzour@gmail.com>
2021-01-09 22:49:30 +0100

    integrate redux to manage the auth state

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+// Components
 import Logo from './../../assets/logo.png';
+import { auth } from './../../firebase/utils';
 
 // Styling
 import './styles.scss';
 
 const Header = (props) => {
+  const { currentUser } = props;
   return (
     <header className="header">
       <div className="logo">
@@ -14,22 +18,47 @@ const Header = (props) => {
           <img src={Logo} alt="logo" />
         </Link>
       </div>
-      <nav className="nav">
-        <ul className="nav-menu">
-          <li className="nav-item">
-            <Link className="nav-link" to="/registration">
-              Register
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/Login">
-              Login
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      {currentUser && (
+        <nav className="nav">
+          <ul className="nav-menu">
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                to="/login"
+                onClick={() => auth.signOut()}
+              >
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+      {!currentUser && (
+        <nav className="nav">
+          <ul className="nav-menu">
+            <li className="nav-item">
+              <Link className="nav-link" to="/registration">
+                Register
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
 
-export default Header;
+Header.defaultProps = {
+  currentUser: null,
+};
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
+export default connect(mapStateToProps, null)(Header);
